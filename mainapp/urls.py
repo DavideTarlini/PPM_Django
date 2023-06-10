62dccb45de68d5dd8553121f4993f15d4861f346
Davide <davide.tarlini@gmail.com>
2023-06-10 21:13:38 +0200

    added basic auth functionalities

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path("", LandingPageView.as_view(), name="landing-page"),
     path("details/<int:pk>", CampaignDetailsView.as_view(), name="campaign-details"),
+    path("sign up/", SignUpView.as_view(), name="sign-up"),
+    path("login/", CustomLoginView.as_view(), name="login"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,8 @@
-from typing import Any
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 from mainapp.models import Campaign, Reward, Comment
 
 class LandingPageView(ListView):
@@ -19,3 +21,15 @@ class CampaignDetailsView(DetailView):
         context["rewards"] = Reward.objects.filter(campaign=pk)
         context["comments"] = Comment.objects.filter(campaign=pk)
         return context
+
+
+class SignUpView(CreateView):
+    template_name = "auth_form.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+
+
+class CustomLoginView(LoginView):
+    template_name = "auth_form.html"
+    success_url = reverse_lazy("landing-page") #scoprire perchè non porta qui, altrimenti
+                                               # ridirezionare all'url di profile

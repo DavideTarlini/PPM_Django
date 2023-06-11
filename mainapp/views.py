390d9c010ece8c431be4f267d98f5fea8cb051c0
Davide <davide.tarlini@gmail.com>
2023-06-11 20:50:56 +0200

    Sign Up bug fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -103,6 +103,10 @@ class SignUpView(FormView):
     template_name = "post_form.html"
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
     
 
 class AccountView(LoginRequiredMixin, TemplateView):
